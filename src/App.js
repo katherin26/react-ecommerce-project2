a0247745f504bb955d0001d36ffcc23a584c92eb
Katherin26 <ft.katheochoa@gmail.com>
2022-07-10 00:11:46 -0400

    added contact me section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Section from "./components/Section/Section";
+import About from "./views/About/About";
+import ContactMe from "./views/ContactMe/ContactMe";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
       <Section />
+      <ContactMe />
       <Footer />
     </div>
   );
---
+++ src/views/ContactMe/ContactMe.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ContactMe.css";
+
+function ContactMe() {
+  return (
+    <div className="contactMe_section">
+      <div className="contactMe_content">Content contact me</div>
+    </div>
+  );
+}
+
+export default ContactMe;
