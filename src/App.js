75c3c70fbb139451b88808dcf91451390d3c48e8
Katherin26 <ft.katheochoa@gmail.com>
2022-07-10 06:36:00 -0400

    added menu header

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <About />
+
       <Section />
+      <About />
       <ContactMe />
       <Footer />
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faD, faP } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div className="header_section">
       <div className="header_content">
-        <h1>Title</h1>
-        <p>Paragraphhhh</p>
+        <div className="header_navbar">
+          <div className="header_logo">
+            <FontAwesomeIcon icon={faD} />
+            <FontAwesomeIcon icon={faP} />
+          </div>
+          <div className="header_menu">
+            <div className="header_menu--bottom">About Us</div>
+            <div className="header_menu--bottom">Products</div>
+            <div className="header_menu--bottom">Contact Us</div>
+          </div>
+        </div>
       </div>
     </div>
   );
