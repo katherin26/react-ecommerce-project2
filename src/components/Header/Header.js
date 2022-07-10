0fa4176fa72ca19ccd61be98440455e671fba08a
Katherin26 <ft.katheochoa@gmail.com>
2022-07-10 19:56:02 -0400

    added menu in mobile version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,14 @@ import { faD, faP } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
-    <div className="header_section">
-      <div className="header_content">
-        <div className="header_navbar">
-          <div className="header_logo">
+    <div className="header__section">
+      <div className="header__content">
+        <div className="header__navbar">
+          <div className="header__logo">
             <FontAwesomeIcon icon={faD} />
             <FontAwesomeIcon icon={faP} />
           </div>
-          <div className="header_menu">
-            <div className="header_menu--bottom">About Us</div>
-            <div className="header_menu--bottom">Products</div>
-            <div className="header_menu--bottom">Contact Us</div>
-          </div>
+          <div className="header__menu">Menu</div>
         </div>
       </div>
     </div>
