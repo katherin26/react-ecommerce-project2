cd41ac3404bd2a420020ba0a69d3fcf69494022e
Katherin26 <ft.katheochoa@gmail.com>
2022-07-10 20:58:15 -0400

    added banner

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,8 +3,17 @@ import "./Section.css";
 
 function Section() {
   return (
-    <div className="product_section">
-      <div className="product_content">Product section content</div>
+    <div className="banner__section">
+      <div className="banner__content">
+        <h2 className="banner__content--title">
+          Regalar algo a una persona es facil, hasta que buscas el mejor regalo
+          para quien te ha robado el corazon.
+        </h2>
+        <h3 className="banner__content--phrase">
+          EL senor el mi pastor y nada me faltara.
+        </h3>
+        <h4 className="banner__content--cite"> Salmo 23:1</h4>
+      </div>
     </div>
   );
 }
