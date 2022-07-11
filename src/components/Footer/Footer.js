a105fc29b0f69b726607a36c3fe996cc22fadc5e
Katherin26 <ft.katheochoa@gmail.com>
2022-07-10 21:20:22 -0400

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
-    <div className="footer_section">
-      <div className="footer_content">
-        <h1>Footer</h1>
-        <p>Footer</p>
+    <div className="footer__section">
+      <div className="footer__content">
+        <h1 className="footer__title">
+          <FontAwesomeIcon className="copyright" icon={faCopyright} /> 2022
+          Dulce Paz | HandCrafted by Katherin Ochoa
+        </h1>
       </div>
     </div>
   );
