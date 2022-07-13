c468177fee52ab1a56bb86ac51a29bf69ce3428c
Katherin26 <ft.katheochoa@gmail.com>
2022-07-13 16:48:35 -0400

    added and fixed some bugs in the header section

--- src/components/Header/HeaderHamburguerMenu.js
+++ src/components/Header/HeaderHamburguerMenu.js
@@ -10,53 +10,47 @@ function HeaderHamburguerMenu() {
   };
 
   return (
-    <div className="hamburgerMenu__section">
-      <div className="hamburgerMenu__content">
-        <div className="hamburgerMenu__container">
-          <div
-            className={
-              showMenu
-                ? "hamburgerMenu__hamburger isactive"
-                : "hamburgerMenu__hamburger"
-            }
-            onClick={handleToggle}
-          >
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
-          </div>
+    <div className="hamburgerMenu__container">
+      <div
+        className={
+          showMenu
+            ? "hamburgerMenu__hamburger isactive"
+            : "hamburgerMenu__hamburger"
+        }
+        onClick={handleToggle}
+      >
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
 
-          <div
-            className={
-              showMenu
-                ? "hamburgerMenu__navbar active"
-                : "hamburgerMenu__navbar "
-            }
-          >
-            <ul>
-              <li>
-                <a href="#">
-                  <b>Home</b>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <b>Home</b>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <b>Home</b>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <b>Home</b>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div
+        className={
+          showMenu ? "hamburgerMenu__navbar active" : "hamburgerMenu__navbar "
+        }
+      >
+        <ul>
+          <li>
+            <a href="#">
+              <b>Productos</b>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <b>Contactame</b>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <b>Historia</b>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <b>Home</b>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
