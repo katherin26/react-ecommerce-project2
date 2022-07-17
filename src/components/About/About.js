16b70f65b003b6e35835becfc3a49c10c75945af
Katherin26 <ft.katheochoa@gmail.com>
2022-07-16 20:54:47 -0400

    added cosmetics images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import Image from "../../assets/images/photo.jpeg";
+import Image from "../../assets/images/dulce_photo.png";
 
 function About() {
   return (
@@ -16,13 +16,14 @@ function About() {
             <img src={Image} className="about__image__user"></img>
           </div>
           <p className="about__paragraph">
-            Fundada en el año 2006, CeTech- OYF inicio como Cibercafé ofreciendo
-            a sus usuarios acceso a internet, llamadas Internacionales y recarga
-            de minutos. Un par de años más tarde fue agregando más servicios
-            como : retiros y consignaciones con el banco Davivienda y Nequi,
-            pago de servicios con Mega red y Puntored, servicios de papelería
-            como : Laminación, Fotocopiadora, Argollado, Impresiones y venta de
-            útiles escolares.
+            Dulce Paz es una compañía fundada en el año 2017 por Jenny
+            Villanueva, nuestra misión es brindar servicios para facilitar a las
+            personas que necesitan de un regalo o una sorpresa especial para un
+            ser querido.
+            <br />
+            Gracias a las redes sociales como Facebook, Instagram, Whatsaap y
+            nuestro sitio web, podemos contactar con personas en toda Colombia y
+            proveer nuestra mercancía y servicios de lunes a domingo.
           </p>
         </div>
       </div>
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,80 +1,100 @@
 import React from "react";
 import "./Dashboard.css";
 import ProductCard from "../../components/Product/ProductCard";
-import Cosmetics from "../../assets/images/photo.jpeg";
-import Practice from "../../assets/images/cetech-oyf.png";
+import Cosmetic1 from "../../assets/images_products_cosmetics/bronzing_chanel_cream.png";
+import Cosmetic2 from "../../assets/images_products_cosmetics/chanel_foundation.png";
+import Cosmetic3 from "../../assets/images_products_cosmetics/chanel_rubor.png";
+import Cosmetic4 from "../../assets/images_products_cosmetics/hoola_benefit.png";
+import Cosmetic5 from "../../assets/images_products_cosmetics/mac_fix_spray.jpeg";
+import Cosmetic6 from "../../assets/images_products_cosmetics/mac_fixplus.png";
+import Cosmetic7 from "../../assets/images_products_cosmetics/mac_lippencil.png";
+import Cosmetic8 from "../../assets/images_products_cosmetics/mac_rubor.png";
+import Cosmetic9 from "../../assets/images_products_cosmetics/mascara_chanel.png";
+import Cosmetic10 from "../../assets/images_products_cosmetics/shadow_chanel_4.png";
 
 let productsDetails = [
   {
     title: "Cosmetics",
     images: [
-      { image: Practice, detail: "detail1" },
-      { image: Practice, detail: "detail2" },
-      { image: Practice, detail: "detail3" },
-      { image: Practice, detail: "detail4" },
-      { image: Practice, detail: "detail5" },
-      { image: Practice, detail: "detail7" },
-      { image: Practice, detail: "detail8" },
+      { image: Cosmetic1, detail: "LES BEIGES Healthy Glow Bronzing Cream" },
+      {
+        image: Cosmetic2,
+        detail: "LES BEIGES Healthy Glow Foundation Hydration and Longwear",
+      },
+      { image: Cosmetic3, detail: "JOUES CONTRASTE Powder Blush" },
+      { image: Cosmetic4, detail: "Hoola Matte Powder Bronzer" },
+      { image: Cosmetic5, detail: "The MAC Fix+Stay" },
+      {
+        image: Cosmetic6,
+        detail: "Prep + Prime Fix + Primer and Setting Spray",
+      },
+      { image: Cosmetic7, detail: "Lip Pencil (Mahogany)" },
+      { image: Cosmetic8, detail: "M-A-C X Stranger Things Powder Blush" },
+      { image: Cosmetic9, detail: "LE VOLUME DE CHANEL Mascara" },
+      {
+        image: Cosmetic10,
+        detail: "LES 4 OMBRES Multi-effect quadra eyeshadow",
+      },
     ],
   },
   {
     title: "Cuidado Facial",
     images: [
-      { image: Practice, detail: "detail1" },
-      { image: Practice, detail: "detail2" },
-      { image: Practice, detail: "detail3" },
-      { image: Practice, detail: "detail4" },
-      { image: Practice, detail: "detail5" },
-      { image: Practice, detail: "detail7" },
-      { image: Practice, detail: "detail8" },
+      { image: Cosmetic1, detail: "detail1" },
+      { image: Cosmetic1, detail: "detail2" },
+      { image: Cosmetic1, detail: "detail3" },
+      { image: Cosmetic1, detail: "detail4" },
+      { image: Cosmetic1, detail: "detail5" },
+      { image: Cosmetic1, detail: "detail7" },
+      { image: Cosmetic1, detail: "detail8" },
     ],
   },
   {
     title: "Desayunos sorpresa",
     images: [
-      { image: Practice, detail: "detail1" },
-      { image: Practice, detail: "detail2" },
-      { image: Practice, detail: "detail3" },
-      { image: Practice, detail: "detail4" },
-      { image: Practice, detail: "detail5" },
-      { image: Practice, detail: "detail7" },
-      { image: Practice, detail: "detail8" },
+      { image: Cosmetic1, detail: "detail1" },
+      { image: Cosmetic1, detail: "detail2" },
+      { image: Cosmetic1, detail: "detail3" },
+      { image: Cosmetic1, detail: "detail4" },
+      { image: Cosmetic1, detail: "detail5" },
+      { image: Cosmetic1, detail: "detail7" },
+      { image: Cosmetic1, detail: "detail8" },
     ],
   },
   {
     title: "Arreglo de Flores",
     images: [
-      { image: Practice, detail: "detail1" },
-      { image: Practice, detail: "detail2" },
-      { image: Practice, detail: "detail3" },
-      { image: Practice, detail: "detail4" },
-      { image: Practice, detail: "detail5" },
-      { image: Practice, detail: "detail7" },
-      { image: Practice, detail: "detail8" },
+      { image: Cosmetic1, detail: "detail1" },
+      { image: Cosmetic1, detail: "detail2" },
+      { image: Cosmetic1, detail: "detail3" },
+      { image: Cosmetic1, detail: "detail4" },
+      { image: Cosmetic1, detail: "detail5" },
+      { image: Cosmetic1, detail: "detail7" },
+      { image: Cosmetic1, detail: "detail8" },
     ],
   },
   {
     title: "Smartwatch",
     images: [
-      { image: Practice, detail: "detail1" },
-      { image: Practice, detail: "detail2" },
-      { image: Practice, detail: "detail3" },
-      { image: Practice, detail: "detail4" },
-      { image: Practice, detail: "detail5" },
-      { image: Practice, detail: "detail7" },
-      { image: Practice, detail: "detail8" },
+      { image: Cosmetic1, detail: "detail1" },
+      { image: Cosmetic1, detail: "detail2" },
+      { image: Cosmetic1, detail: "detail3" },
+      { image: Cosmetic1, detail: "detail4" },
+      { image: Cosmetic1, detail: "detail5" },
+      { image: Cosmetic1, detail: "detail7" },
+      { image: Cosmetic1, detail: "detail8" },
     ],
   },
   {
     title: "Bolsos",
     images: [
-      { image: Practice, detail: "detail1" },
-      { image: Practice, detail: "detail2" },
-      { image: Practice, detail: "detail3" },
-      { image: Practice, detail: "detail4" },
-      { image: Practice, detail: "detail5" },
-      { image: Practice, detail: "detail7" },
-      { image: Practice, detail: "detail8" },
+      { image: Cosmetic1, detail: "detail1" },
+      { image: Cosmetic1, detail: "detail2" },
+      { image: Cosmetic1, detail: "detail3" },
+      { image: Cosmetic1, detail: "detail4" },
+      { image: Cosmetic1, detail: "detail5" },
+      { image: Cosmetic1, detail: "detail7" },
+      { image: Cosmetic1, detail: "detail8" },
     ],
   },
 ];
